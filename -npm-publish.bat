2f029f4b9b723e277ecead9d0d2f3400406a701a
tom <tom@sapletta.com>
2019-06-03 13:01:12 +0200

    first version

--- -npm-publish.bat
+++ -npm-publish.bat
@@ -3,4 +3,5 @@
 :: Zrobic opcje do zapisywania wersji jesli sie chce inna wersje jako kolejna
 set /P VERSION= < VERSION.txt
 git push origin v%VERSION%
-npm publish --access public && @ping -n 8 localhost> nul && echo "Optionally put Your defined Version: X.Y.Z" && -version.bat && start firefox "https://www.npmjs.com/package/gitpad-each" && -ticket.bat
+npm publish --access public && @ping -n 8 localhost> nul && echo "Optionally put Your defined Version: X.Y.Z" && start firefox "https://www.npmjs.com/package/gitpad-empty"
+::&& -ticket.bat
